fed5e9c62e3c52358b311f1348dbe1460c5314d3
Programmers <prog-hp1@core2062.com>
2014-02-01 09:56:38 -0600

    Started autonomous code

---
+++ vision.cpp
@@ -0,0 +1,89 @@
+#include "vision.h"	
+
+PixelValue pixVal(unsigned int r, unsigned int g, unsigned int b, unsigned int a) {
+		PixelValue ret;
+		ret.grayscale = 14145495;
+		ret.rgb.R = r;
+		ret.rgb.G = g;
+		ret.rgb.B = b;
+		ret.rgb.alpha = a;
+		cout << (int)ret.rgb.R << " " << (int)ret.rgb.G << " " << (int)ret.rgb.B << endl;
+		return ret;
+	}
+	
+#define chk(input) {if(!(input)) {goto error;}}
+#define log(im) {cout<<"writing "<<step<<endl; im.Write(("/."+step+".jpg").c_str());}
+	
+	void visionMain(){
+		cout << "starting autonomous" << endl;
+		AxisCamera& camera = AxisCamera::GetInstance("10.20.62.11");
+		camera.WriteBrightness(10);
+		camera.GetNewImageSem();
+		std::string step = "start";
+		ColorImage image (IMAQ_IMAGE_RGB);
+		camera.GetImage(&image);
+		log(image);
+		
+		step = "thresh";
+		BinaryImage* thresholdImage = image.ThresholdRGB(0, 23, 22, 50, 0, 43);
+		log((*thresholdImage));
+		
+		step = "small";
+		BinaryImage* small = thresholdImage->RemoveSmallObjects(true, 1);
+		log((*small));
+		
+		vector<ParticleAnalysisReport>* reports = small->GetOrderedParticleAnalysisReports();
+		vector<ParticleAnalysisReport>::const_iterator i;
+		for(i = reports->begin(); i!=reports->end(); i++){
+			cout<< i->boundingRect.top <<" "<< i->boundingRect.left << endl;
+		}
+		
+		
+		delete thresholdImage;
+		delete small;
+		delete reports;
+		AxisCamera::DeleteInstance();
+	}
+	void TestSubtraction()
+	{
+		ColorImage image (IMAQ_IMAGE_RGB);
+		ColorImage output (IMAQ_IMAGE_RGB);
+		Image* imaqImage = image.GetImaqImage();
+		Image* imaqOutput = output.GetImaqImage();
+		
+		std::string step = "start";
+		chk(imaqReadFile(imaqImage, "/Center.jpg", NULL, NULL));
+		chk(imaqReadFile(imaqOutput, "/Center.jpg", NULL, NULL));
+		log(image);
+		
+		PixelValue darkening_cnst;
+		
+		step = "red";
+		darkening_cnst = pixVal(255, 0, 0, 0);
+		chk(imaqSubtractConstant(imaqImage, imaqOutput, darkening_cnst));
+		log(image);
+		
+		step = "green";
+		darkening_cnst = pixVal(0, 255, 0, 0);
+		chk(imaqSubtractConstant(imaqImage, imaqOutput, darkening_cnst));
+		log(image);
+		
+		step = "blue";
+		darkening_cnst = pixVal(0, 0, 255, 0);
+		chk(imaqSubtractConstant(imaqImage, imaqOutput, darkening_cnst));
+		log(image);
+		
+		step = "alpha";
+		darkening_cnst = pixVal(0, 0, 0, 255);
+		chk(imaqSubtractConstant(imaqImage, imaqOutput, darkening_cnst));
+		log(image);
+		
+		step = "grey";
+		darkening_cnst = pixVal(215, 215, 215, 0);
+		chk(imaqSubtractConstant(imaqImage, imaqOutput, darkening_cnst));
+		log(image);
+		
+		return;
+		error:
+			cout << "Error in step " << step << endl;
+	}
---
+++ vision.h
@@ -0,0 +1,11 @@
+#ifndef VISION_H
+#define VISION_H
+
+#include "WPILib.h"
+
+namespace CORE{
+	void visionMain();
+	void TestSubtraction();
+}
+
+#endif
