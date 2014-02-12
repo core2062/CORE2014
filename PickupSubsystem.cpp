8a6e52f52e086e560de3df9eab8de97c61f70a63
Programmers <prog-hp1@core2062.com>
2014-02-11 19:26:17 -0600

    Test and integrate pickup
    Works! :)

--- PickupSubsystem.cpp
+++ PickupSubsystem.cpp
@@ -9,54 +9,37 @@ void PickupSubsystem::robotInit(void){
 void PickupSubsystem::teleopInit(void){
 	robot.compressor->Start();
 	
-	robot.joystick.register_button("pickup-out", 2, 7);
-	robot.joystick.register_button("pickup-in", 2, 8);
-	robot.joystick.register_button("roller-in", 2, 9);
-	robot.joystick.register_button("roller-out", 2, 10);
+	robot.joystick.register_button("pickup-out", 2, 7, JoystickCache::RISING);
+	robot.joystick.register_button("pickup-in", 2, 8, JoystickCache::RISING);
+	robot.joystick.register_button("roller-in", 2, 9, JoystickCache::RISING);
+	robot.joystick.register_button("roller-out", 2, 10, JoystickCache::RISING);
 
 }
 void PickupSubsystem::teleop(void){
-	double pickup_speed = 1; //SmartDashboard::GetNumber("pickup-speed");
 	double roller_speed = SmartDashboard::GetNumber("roller-speed");
-	double pickup_output = 0;
 	double roller_output = roller_motor.Get();
 	
-	if (robot.joystick.button("pickup-out") && !pickup_limit_out.Get()){
-		pickup_output = pickup_speed;
-	
-	}
-	
-	else if (robot.joystick.button("pickup-in") && !pickup_limit_in.Get()){
-		pickup_output = -pickup_speed;
-	
-	}
-	
-	if (robot.joystick.button("roller-in") && !old_roller_in){
-		if (roller_motor.Get() <= 0){
-			roller_output = roller_speed;
-		} else { 
-			roller_output = 0;
-		}
-	} else if (robot.joystick.button("roller-out") && !old_roller_out){
+	if (robot.joystick.button("roller-in")){
 		if (roller_motor.Get() >= 0){
 			roller_output = -roller_speed;
 		} else { 
 			roller_output = 0;
 		}
+	} else if (robot.joystick.button("roller-out")){
+		if (roller_motor.Get() <= 0){
+			roller_output = roller_speed;
+		} else { 
+			roller_output = 0;
+		}
 	}
 	
-	old_roller_out = robot.joystick.button("roller-out");
-	old_roller_in = robot.joystick.button("roller-in");
-	
 	//pickup_motor.Set(pickup_output);
-	if (pickup_output > 0 ){
-		pickup_solenoid_left.Set(DoubleSolenoid::kForward);
-		pickup_solenoid_right.Set(DoubleSolenoid::kForward);
-
-	}else	if (pickup_output < 0 ){
+	if (robot.joystick.button("pickup-out")){
 		pickup_solenoid_left.Set(DoubleSolenoid::kReverse);
 		pickup_solenoid_right.Set(DoubleSolenoid::kReverse);
-
+	}else if (robot.joystick.button("pickup-in")){
+		pickup_solenoid_left.Set(DoubleSolenoid::kForward);
+		pickup_solenoid_right.Set(DoubleSolenoid::kForward);
 	} else {
 		pickup_solenoid_left.Set(DoubleSolenoid::kOff);
 	    pickup_solenoid_right.Set(DoubleSolenoid::kOff);
--- PickupSubsystem.h
+++ PickupSubsystem.h
@@ -10,11 +10,6 @@ class PickupSubsystem : public CORESubsystem {
 	Victor roller_motor;
 	DoubleSolenoid pickup_solenoid_left;
 	DoubleSolenoid pickup_solenoid_right;
-	DigitalInput pickup_limit_in;
-	DigitalInput pickup_limit_out;
-
-	bool old_roller_in;
-	bool old_roller_out;
 	
 public:
 	std::string name(void){
@@ -23,14 +18,11 @@ public:
 	
 	PickupSubsystem(CORERobot& robot):
 		CORESubsystem(robot),
-		roller_motor(10),
-		pickup_solenoid_left(1,2), //module 2 removed for testing
-		pickup_solenoid_right(2,3,4),
-		pickup_limit_in(7),
-		pickup_limit_out(8)
+		roller_motor(5),
+		pickup_solenoid_left(1,3,4), //module 2 removed for testing,
+		pickup_solenoid_right(2,3,4)
 	{
-		old_roller_in = false;
-		old_roller_out = false;
+		
 	}
 	
 	void robotInit(void);
